aa08e7648a2ff95724ddc746e19c6683c4b80ee8
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-29 15:08:12 -0400

    lint and format

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
-const button = document.querySelector("body");
+const button = document.querySelector('body');
 
-button.addEventListener("click", () => {
-	button.classList.toggle("dark-mode");
-}, false);
+button.addEventListener(
+    'click',
+    () => {
+      button.classList.toggle('dark-mode');
+    },
+    false,
+);
